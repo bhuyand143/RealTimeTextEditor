04d602cda62dd234dc3e73184a3de890edecd707
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2024-01-25 23:55:44 +0530

    m

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ mongoose.connect(mongourl)
 
 const io = require("socket.io")(3001, {
   cors: {
-    origin: process.env.CORS_URL,
+    origin: "https://collab-text.vercel.app/",
     methods: ["GET", "POST"],
   },
 })
